0a9ab33e5ee0e88dd07cee4478fc9031d0bea720
Roman-It-star <romanop2014@gmail.com>
2025-05-04 14:18:52 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.базаДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.соединитьсяСБазойToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отключитьСоединениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnLoadProducts = new System.Windows.Forms.Button();
+            this.listViewProducts = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnTransaction = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.btnEditProduct = new System.Windows.Forms.Button();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.базаДанныхToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(812, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // базаДанныхToolStripMenuItem
+            // 
+            this.базаДанныхToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.соединитьсяСБазойToolStripMenuItem,
+            this.отключитьСоединениеToolStripMenuItem});
+            this.базаДанныхToolStripMenuItem.Name = "базаДанныхToolStripMenuItem";
+            this.базаДанныхToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.базаДанныхToolStripMenuItem.Text = "База данных";
+            // 
+            // соединитьсяСБазойToolStripMenuItem
+            // 
+            this.соединитьсяСБазойToolStripMenuItem.Name = "соединитьсяСБазойToolStripMenuItem";
+            this.соединитьсяСБазойToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.соединитьсяСБазойToolStripMenuItem.Text = "Соединиться с базой";
+            this.соединитьсяСБазойToolStripMenuItem.Click += new System.EventHandler(this.соединитьсяСБазойToolStripMenuItem_Click);
+            // 
+            // отключитьСоединениеToolStripMenuItem
+            // 
+            this.отключитьСоединениеToolStripMenuItem.Name = "отключитьСоединениеToolStripMenuItem";
+            this.отключитьСоединениеToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.отключитьСоединениеToolStripMenuItem.Text = "Отключить соединение";
+            this.отключитьСоединениеToolStripMenuItem.Click += new System.EventHandler(this.отключитьСоединениеToolStripMenuItem_Click);
+            // 
+            // btnLoadProducts
+            // 
+            this.btnLoadProducts.Location = new System.Drawing.Point(13, 52);
+            this.btnLoadProducts.Name = "btnLoadProducts";
+            this.btnLoadProducts.Size = new System.Drawing.Size(110, 23);
+            this.btnLoadProducts.TabIndex = 1;
+            this.btnLoadProducts.Text = "Список продуктов";
+            this.btnLoadProducts.UseVisualStyleBackColor = true;
+            this.btnLoadProducts.Click += new System.EventHandler(this.btnLoadProducts_Click);
+            // 
+            // listViewProducts
+            // 
+            this.listViewProducts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewProducts.HideSelection = false;
+            this.listViewProducts.Location = new System.Drawing.Point(13, 96);
+            this.listViewProducts.Name = "listViewProducts";
+            this.listViewProducts.Size = new System.Drawing.Size(366, 201);
+            this.listViewProducts.TabIndex = 2;
+            this.listViewProducts.UseCompatibleStateImageBehavior = false;
+            this.listViewProducts.View = System.Windows.Forms.View.Details;
+            this.listViewProducts.DoubleClick += new System.EventHandler(this.listViewProducts_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Цена";
+            this.columnHeader2.Width = 80;
+            // 
+            // btnTransaction
+            // 
+            this.btnTransaction.Location = new System.Drawing.Point(155, 52);
+            this.btnTransaction.Name = "btnTransaction";
+            this.btnTransaction.Size = new System.Drawing.Size(110, 23);
+            this.btnTransaction.TabIndex = 3;
+            this.btnTransaction.Text = "Транзакция";
+            this.btnTransaction.UseVisualStyleBackColor = true;
+            this.btnTransaction.Click += new System.EventHandler(this.btnTransaction_Click);
+            // 
+            // btnDeleteProduct
+            // 
+            this.btnDeleteProduct.Location = new System.Drawing.Point(455, 52);
+            this.btnDeleteProduct.Name = "btnDeleteProduct";
+            this.btnDeleteProduct.Size = new System.Drawing.Size(110, 23);
+            this.btnDeleteProduct.TabIndex = 4;
+            this.btnDeleteProduct.Text = "Удалить продукт";
+            this.btnDeleteProduct.UseVisualStyleBackColor = true;
+            this.btnDeleteProduct.Click += new System.EventHandler(this.btnDeleteProduct_Click);
+            // 
+            // btnEditProduct
+            // 
+            this.btnEditProduct.Location = new System.Drawing.Point(611, 52);
+            this.btnEditProduct.Name = "btnEditProduct";
+            this.btnEditProduct.Size = new System.Drawing.Size(110, 23);
+            this.btnEditProduct.TabIndex = 5;
+            this.btnEditProduct.Text = "Редактировать продукт";
+            this.btnEditProduct.UseVisualStyleBackColor = true;
+            this.btnEditProduct.Click += new System.EventHandler(this.btnEditProduct_Click);
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(465, 115);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(611, 114);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(487, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Цена";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(465, 186);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategory.TabIndex = 10;
+            this.cmbCategory.SelectedIndexChanged += new System.EventHandler(this.cmbCategory_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Категория";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Категория";
+            this.columnHeader3.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 355);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.btnEditProduct);
+            this.Controls.Add(this.btnDeleteProduct);
+            this.Controls.Add(this.btnTransaction);
+            this.Controls.Add(this.listViewProducts);
+            this.Controls.Add(this.btnLoadProducts);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "База рынков";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem базаДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem соединитьсяСБазойToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отключитьСоединениеToolStripMenuItem;
+        private System.Windows.Forms.Button btnLoadProducts;
+        private System.Windows.Forms.ListView listViewProducts;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnTransaction;
+        private System.Windows.Forms.Button btnDeleteProduct;
+        private System.Windows.Forms.Button btnEditProduct;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,358 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlConnection connection = new NpgsqlConnection();
+        string connectionString = ConfigurationManager.ConnectionStrings["YandexCloudConnection"].ConnectionString;
+        public Form1()
+        {
+            connection.StateChange += Connection_StateChange; //для активности и неактивности пункта меню в зависимости от соединения
+            InitializeComponent();
+        }
+
+        //для активности и неактивности пункта меню в зависимости от соединения
+        private void Connection_StateChange(object sender, StateChangeEventArgs e)
+        {
+            соединитьсяСБазойToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Closed);
+            отключитьСоединениеToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Open);
+        }
+
+        private void соединитьсяСБазойToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+
+                    LoadCategories();
+                }
+                else
+                {
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+                }
+            }
+            catch (PostgresException ex)
+            {
+                MessageBox.Show($"PostgreSQL ошибка: {ex.MessageText}\nКод: {ex.SqlState}",
+                    "Ошибка PostgreSQL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show($"Ошибка уровня подключения: {ex.Message}",
+                                "Ошибка Npgsql", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Неизвестная ошибка: {ex.Message}",
+                                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void отключитьСоединениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+            {
+                MessageBox.Show("Соединение уже закрыто");
+            }
+        }
+
+        private class ComboBoxItem
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+            public override string ToString() => Text;
+        }
+
+        private void LoadCategories()
+        {
+            cmbCategory.Items.Clear();
+
+            // Добавляем специальный пункт "Все категории"
+            cmbCategory.Items.Add(new ComboBoxItem { Text = "Все категории", Value = DBNull.Value });
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = @"SELECT categoryid, categoryname FROM ""category""";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        cmbCategory.Items.Add(new ComboBoxItem
+                        {
+                            Text = reader["categoryname"].ToString(),
+                            Value = reader["categoryid"]
+                        });
+                    }
+                }
+            }
+            if (cmbCategory.Items.Count > 0)
+                cmbCategory.SelectedIndex = 0;
+        }
+        private void btnLoadProducts_Click(object sender, EventArgs e)
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Сначала подключитесь к базе данных.");
+                return;
+            }
+
+            try
+            {
+                // Очищаем старый список
+                listViewProducts.Items.Clear();
+
+                // Создаём команду
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                SELECT p.productid, p.productname, p.unitprice, c.categoryname
+                FROM product p
+                LEFT JOIN ""category"" c ON p.categoryid = c.categoryid";
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var item = new ListViewItem(reader["productname"].ToString()); // 1-я колонка
+                            item.SubItems.Add(reader["unitprice"].ToString());             // 2-я колонка
+                            item.SubItems.Add(reader["categoryname"].ToString());         // 3-я колонка
+                            item.Tag = reader["productid"];
+
+                            listViewProducts.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при получении данных: " + ex.Message);
+            }
+        }
+
+        private void btnTransaction_Click(object sender, EventArgs e)
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Сначала подключитесь к базе данных.");
+                return;
+            }
+
+            // Начинаем транзакцию
+            var transaction = connection.BeginTransaction();
+
+            try
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.Transaction = transaction;
+
+                    // Вставка первого продукта
+                    // ⚠️ НЕ УКАЗЫВАЕМ productid — он автоинкремент
+                    command.CommandText = @"INSERT INTO ""product"" 
+(productname, supplierid, categoryid, quantityperunit, unitprice, unitsinstock, unitsonorder, reorderlevel, discontinued)
+VALUES ('Auto Product A', 1, 1, '12 bottles', 12.5, 10, 0, 0, 0);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"INSERT INTO ""product"" 
+(productname, supplierid, categoryid, quantityperunit, unitprice, unitsinstock, unitsonorder, reorderlevel, discontinued)
+VALUES ('Auto Product B', 1, 1, '6 bottles', 8.5, 7, 0, 0, 0);";
+                    command.ExecuteNonQuery();
+
+                    // Коммитим (подтверждаем)
+                    transaction.Commit();
+                    MessageBox.Show("Оба продукта успешно добавлены в базу.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при вставке данных: " + ex.Message);
+
+                try
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("Транзакция отменена.");
+                }
+                catch (Exception rollbackEx)
+                {
+                    MessageBox.Show("Ошибка при откате транзакции: " + rollbackEx.Message);
+                }
+            }
+
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            if (listViewProducts.SelectedItems.Count == 0)
+                return;
+
+            int productId = (int)listViewProducts.SelectedItems[0].Tag;
+
+            var result = MessageBox.Show("Удалить товар?", "Удаление", MessageBoxButtons.YesNo);
+            if (result != DialogResult.Yes)
+                return;
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "DELETE FROM \"product\" WHERE productid = @id";
+                command.Parameters.AddWithValue("id", productId);
+
+                int rows = command.ExecuteNonQuery();
+                MessageBox.Show(rows > 0 ? "Удалено!" : "Не найдено.");
+                btnLoadProducts.PerformClick();
+            }
+        }
+
+        private void btnEditProduct_Click(object sender, EventArgs e)
+        {
+            if (listViewProducts.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Выберите товар в списке.");
+                return;
+            }
+
+            string newName = txtProductName.Text;
+            if (!decimal.TryParse(txtPrice.Text, out decimal newPrice))
+            {
+                MessageBox.Show("Цена некорректна.");
+                return;
+            }
+
+            int productId = (int)listViewProducts.SelectedItems[0].Tag;
+            int categoryId = (int)((ComboBoxItem)cmbCategory.SelectedItem).Value;
+
+            try
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                UPDATE ""product""
+                SET productname = @name, unitprice = @price, categoryid = @cat
+                WHERE productid = @id";
+
+                    command.Parameters.AddWithValue("name", newName);
+                    command.Parameters.AddWithValue("price", newPrice);
+                    command.Parameters.AddWithValue("cat", categoryId);
+                    command.Parameters.AddWithValue("id", productId);
+
+                    int rows = command.ExecuteNonQuery();
+
+                    MessageBox.Show(rows > 0 ? "Обновлено!" : "Продукт не найден.");
+                    btnLoadProducts.PerformClick();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка обновления: " + ex.Message);
+            }
+        }
+
+        private void listViewProducts_DoubleClick(object sender, EventArgs e)
+        {
+            if (listViewProducts.SelectedItems.Count == 0)
+                return;
+
+            var selectedItem = listViewProducts.SelectedItems[0];
+
+            // Получаем текст из колонок
+            string productName = selectedItem.SubItems[0].Text;
+            string unitPrice = selectedItem.SubItems[1].Text;
+
+            // Подставим значения в поля
+            txtProductName.Text = productName;
+            txtPrice.Text = unitPrice;
+
+            // Получаем productid из Tag
+            int productId = (int)selectedItem.Tag;
+
+            // Получаем categoryid по productid
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT categoryid FROM \"product\" WHERE productid = @id";
+                command.Parameters.AddWithValue("id", productId);
+
+                var categoryId = command.ExecuteScalar();
+
+                // Выбираем нужный элемент в ComboBox по значению
+                for (int i = 0; i < cmbCategory.Items.Count; i++)
+                {
+                    if (((ComboBoxItem)cmbCategory.Items[i]).Value.ToString() == categoryId.ToString())
+                    {
+                        cmbCategory.SelectedIndex = i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //загружает данные для фильтрации по категориям
+        private void LoadProductsByCategory()
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Сначала подключитесь к базе данных.");
+                return;
+            }
+
+            listViewProducts.Items.Clear();
+
+            var selectedCategory = (ComboBoxItem)cmbCategory.SelectedItem;
+
+            using (var command = connection.CreateCommand())
+            {
+                if (selectedCategory.Value == DBNull.Value)
+                {
+                    // Без фильтра — все продукты
+                    command.CommandText = @"
+                SELECT p.productid, p.productname, p.unitprice, c.categoryname
+                FROM product p
+                LEFT JOIN ""category"" c ON p.categoryid = c.categoryid";
+                }
+                else
+                {
+                    command.CommandText = @"
+                SELECT p.productid, p.productname, p.unitprice, c.categoryname
+                FROM product p
+                LEFT JOIN ""category"" c ON p.categoryid = c.categoryid
+                WHERE p.categoryid = @catid";
+                    command.Parameters.AddWithValue("catid", selectedCategory.Value);
+                }
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var item = new ListViewItem(reader["productname"].ToString());
+                        item.SubItems.Add(reader["unitprice"].ToString());
+                        item.SubItems.Add(reader["categoryname"].ToString());
+                        item.Tag = reader["productid"];
+                        listViewProducts.Items.Add(item);
+                    }
+                }
+            }
+        }
+        private void cmbCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadProductsByCategory();
+        }
+    }
+}
